c7cdd1adcd741796bb327e4507defd3b0b481211
soapman <33997593+soapmancn@users.noreply.github.com>
2022-12-24 13:04:45 +0800

    Update salary.py

--- salary.py
+++ salary.py
@@ -40,6 +40,8 @@ driver.find_element(By.ID, 'PassWord').send_keys(os.environ['PASS_WORD'])
 driver.find_element(By.ID, 'txtCheckCode').send_keys(checkText)
 driver.find_element(By.ID, 'ImageButton2').click()
 
+count = len(driver.find_elements(By.TAG_NAME, 'tr'))
+
 # 解析
 # 表头
 th_list = []
@@ -47,45 +49,49 @@ thName = driver.find_elements(By.TAG_NAME, 'tr').__getitem__(6).find_elements(By
 for i in thName:
     th_list.append(i.text)
 # 表内容
-td_list = []
-tdContent = driver.find_element(By.ID, 'GridView1_0').find_elements(By.TAG_NAME, 'td')
-for i in tdContent:
-    td_list.append(i.text)
+td_arrays = []
+for num in range(7, count):
+    td_list = []
+    tdContent = driver.find_elements(By.TAG_NAME, 'tr').__getitem__(num).find_elements(By.TAG_NAME, 'td')
+    for i in tdContent:
+        td_list.append(i.text)
+    td_arrays.append(td_list)
 driver.quit()
 
 # 组装消息
-message = ''
-fgx = '-----------------------'
-hhf = '\n'
-year = ''
-month = ''
-for i, val in enumerate(th_list):
-    if td_list[i] != '0.00' and val != '公司名称' and val != '人员':
-        message += val + ':' + td_list[i] + hhf
-    if val == '计发工资' or val == '企业社保小计' or val == '个人扣保险及住房合计' or val == '累计住房租金支出扣除':
-        message += fgx + hhf
-    if val == '工资年度':
-        year = td_list[i]
-    if val == '工资月份':
-        month = td_list[i]
+for td_list in td_arrays:
+    message = ''
+    fgx = '-----------------------'
+    hhf = '\n'
+    year = ''
+    month = ''
+    for i, val in enumerate(th_list):
+        if td_list[i] != '0.00' and val != '公司名称' and val != '人员':
+            message += val + ':' + td_list[i] + hhf
+        if val == '计发工资' or val == '企业社保小计' or val == '个人扣保险及住房合计' or val == '累计住房租金支出扣除':
+            message += fgx + hhf
+        if val == '工资年度':
+            year = td_list[i]
+        if val == '工资月份':
+            month = td_list[i]
 
-# 查询数据库入库
-conn = mysql.connector.connect(host=os.environ['MYSQL_HOST'], user='root', passwd=os.environ['MYSQL_PASS'], db='mysql')
-cursor = conn.cursor()
-sql = 'select count(*) from my_salary where year = %s and month = %s'
-val = (year, month)
-cursor.execute(sql, val)
-count = cursor.fetchone()[0]
-if count < 1:
-    # 发送通知
-    token = os.environ['TEL_TOKEN']
-    chat_id = os.environ['CHAT_ID']
-    r = requests.post(f'https://api.telegram.org/bot{token}/sendMessage', json={"chat_id": chat_id, "text": message})
-    # 入库
-    insertSql = 'insert into my_salary(message, year, month) values(%s, %s, %s)'
-    insertVal = (message, year, month)
-    cursor.execute(insertSql, insertVal)
-    conn.commit()
-    conn.close()
+    # 查询数据库入库
+    conn = mysql.connector.connect(host=os.environ['MYSQL_HOST'], user='root', passwd=os.environ['MYSQL_PASS'], db='mysql')
+    cursor = conn.cursor()
+    sql = 'select count(*) from my_salary where year = %s and month = %s'
+    val = (year, month)
+    cursor.execute(sql, val)
+    count = cursor.fetchone()[0]
+    if count < 1:
+        # 发送通知
+        token = os.environ['TEL_TOKEN']
+        chat_id = os.environ['CHAT_ID']
+        r = requests.post(f'https://api.telegram.org/bot{token}/sendMessage', json={"chat_id": chat_id, "text": message})
+        # 入库
+        insertSql = 'insert into my_salary(message, year, month) values(%s, %s, %s)'
+        insertVal = (message, year, month)
+        cursor.execute(insertSql, insertVal)
+        conn.commit()
+        conn.close()
 
 
